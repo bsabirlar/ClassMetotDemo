4406128442049f5ebc5e456d6f718f796485e798
buraksabrlar <b.sabirlarr@gmail.com>
2021-02-16 13:01:11 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class ProductManager
+    {
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Yeni Müşteri Eklendi. : " + product.FirstName);
+        }
+
+        public void List(Product product)
+        {
+            Console.WriteLine("Müşteriler Listesi : " + product.Id + " " + product.FirstName);
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("delete");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,33 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.FirstName = "Halil";
+            product1.LastName = "Nerber";
+            product1.İl = "Samsun";
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.FirstName = "Salih";
+            product2.LastName = "Mener";
+            product2.İl = "Ankara";
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.FirstName);
+                Console.WriteLine(product.LastName);
+                Console.WriteLine(product.İl);
+                Console.WriteLine(product.FirstName);
+            }
+
+            Console.WriteLine("-- ekleme --");
+            ProductManager manager = new ProductManager();
+            manager.Ekle(product1);
+            manager.Ekle(product2);
+
         }
     }
 }
